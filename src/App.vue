2b0f39d98de92d1ee0a943cdc2e43a209e6221ba
Nauman-Arshad <naumankhan642@gmail.com>
2022-09-21 19:35:07 +0500

    replace option using ref in vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Data/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Data from './components/Data.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Data
+}
 }
 </script>
 
---
+++ src/components/Data.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h4>Options - {{c_fname}}</h4>
+    <h4>Composition -{{o_fname}}</h4>
+    <h4>Composition - {{greet}}</h4>
+    </div>
+</template>
+
+<script>
+    import {ref} from 'vue'
+    export default {
+        name: 'DataComponent',
+        setup(){
+            const c_fname = ref('Clark')
+            c_fname.value = 'Lara'
+            const greet = `Hello, ${c_fname.value}`
+            console.log('c_fname', c_fname)
+            return {
+                c_fname,
+                greet,
+            }
+        },
+        data(){
+            return {
+                o_fname: 'Nauman'
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
